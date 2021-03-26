1a9461ed3670e16bb31b9a5330b179ac0c4c8116
paulasm13 <paula13sepulved@hotmail.com>
2021-03-26 20:44:54 +0100

    Ejercicio 3 - L5.

---
+++ P2/S4/botón1.js
@@ -0,0 +1,19 @@
+//-- Manejador del evento click sobre el boton 
+//-- Cada vez aue se hace click en el se invoca a esta funcion
+function manejador_boton()
+{
+  paragraph.innerHTML += "1"   //-- Se suma un 1 al párrafo 
+  console.log('Botón pulsado')
+}
+
+console.log("Ejecutando js...")
+
+//-- Leer el boton identificado como test
+const button = document.getElementById('button')
+const paragraph = document.getElementById('paragraph')  //-- El párrafo obtiene su valor inicial que es ""
+
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón: que se ejecute la
+//-- funcion manejador_boton()
+button.onclick = manejador_boton;
