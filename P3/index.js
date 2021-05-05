44b43c2d43e08e05c3b1d137911fec643521dcd5
paulasm13 <paula13sepulved@hotmail.com>
2021-05-05 23:58:30 +0200

    Destruye ladrillos - detecta colisiones

--- P3/index.js
+++ P3/index.js
@@ -16,14 +16,16 @@ var paddleWidth = 65;
 var paddle = (canvas.width - paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
-var brickRows = 5; // 5 filas y 9 columnas!
-var brickColumns = 9;
-var brickWidth = 50;    
-var brickHeight = 20;
-var brickPadding = 8;    // Espacio entre ladrillos
-var brickMarginTop = 40;
-var brickMarginLeft = 15;
-var ballSize = 10;
+const BRICK = {
+    rows: 7,
+    columns: 9,
+    width: 50,
+    height: 20,
+    padding: 8,
+    marginTop: 40,
+    marginLeft: 15,
+    show: true
+};
 
 let x = canvas.width/2;
 let y = canvas.height-190;
@@ -71,26 +73,44 @@ function booleanPaddle() {
 
 // Implemento ladrillos en matriz bidimensional
 var bricks = [];
-for (i=0; i<brickColumns; i++) {
+for (i=0; i<BRICK.columns; i++) {
     bricks[i] = [];
-    for(j=0; j<brickRows; j++) {
-        bricks[i][j] = {x: 0, y: 0};    // Inicializo la posición de cada ladrillo
+    for(j=0; j<BRICK.rows; j++) {
+        bricks[i][j] = {
+            x: (i*(BRICK.width+BRICK.padding))+BRICK.marginLeft,
+            y: (j*(BRICK.height+BRICK.padding))+BRICK.marginTop,
+            visible: BRICK.show
+        };  // Asocio a cada ladrillo su posición y su visibilidad (true)
     }
 }
 
 // Dibujo ladrillos
 function drawBricks() {
-    for(i=0; i<brickColumns; i++) {
-        for(j=0; j<brickRows; j++) {
-            var brickX = (i*(brickWidth+brickPadding))+brickMarginLeft;     // Asocio a cada ladrillo
-            var brickY = (j*(brickHeight+brickPadding))+brickMarginTop;     // su posición
-            bricks[i][j].x = brickX;
-            bricks[i][j].y = brickY;
-            ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);              // Estilo del ladrillo
+    collisions();
+    for(i=0; i<BRICK.columns; i++) {
+        for(j=0; j<BRICK.rows; j++) {
+            if(bricks[i][j].visible){
+              ctx.beginPath();
+                ctx.rect(bricks[i][j].x, bricks[i][j].y, BRICK.width, BRICK.height);              // Estilo del ladrillo
                 ctx.fillStyle = '#851B03';
                 ctx.fill();
-            ctx.closePath();        
+            ctx.closePath();  
+            }       
+        }
+    }
+}
+
+// Detecto colisiones 
+function collisions() {
+    for(i=0; i<BRICK.columns; i++) {
+        for(j=0; j<BRICK.rows; j++) {
+            var l = bricks[i][j];
+            if(l.visible == true) {
+                if(x > l.x && x < l.x + BRICK.width && y > l.y && y < l.y + BRICK.height) {
+                    vely = -vely;
+                    l.visible = false;
+                }
+            }
         }
     }
 }
