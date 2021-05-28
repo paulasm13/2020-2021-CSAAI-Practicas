d26292037a849e575d8a264af8a4580f80401e29
paulasm13 <paula13sepulved@hotmail.com>
2021-05-28 23:39:40 +0200

    Mejorando CSS

--- P4/index.js
+++ P4/index.js
@@ -2,25 +2,23 @@ console.log("Ejecutando JS....")
 
 //-- Obtener elementos del DOM
 const canvas = document.getElementById('canvas');
-const img = document.getElementById('imagesrc')
+const img1 = document.getElementById('imagesrc1');
+const img2 = document.getElementById('imagesrc2');
 const ctx = canvas.getContext('2d');
 
-//-- Función de retrollamada de imagen cargada
-//-- La imagen no se carga instantaneamente, sino que
-//-- lleva un tiempo. Sólo podemos acceder a ella una vez
-//-- que esté totalmente cargada
-img.onload = function () {
+//-- Para la imagen uno
+img1.onload = function () {
 
   console.log("Imagen cargada");
 
   //-- Se establece como tamaño del canvas el mismo
   //-- que el de la imagen original
-  canvas.width = img.width;
-  canvas.height = img.height;
+  canvas.width = img1.width;
+  canvas.height = img1.height;
 
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavia
-  ctx.drawImage(img, 0,0);
+  ctx.drawImage(img1, 0,0);
 };
 
 console.log("Fin...");
