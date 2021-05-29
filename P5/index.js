5f81c4e903cbbb0a0c5432d1cf886e2e4a764db6
paulasm13 <paula13sepulved@hotmail.com>
2021-05-29 18:55:26 +0200

    Videos

--- P5/index.js
+++ P5/index.js
@@ -1,4 +1,4 @@
-//----- Obtener elemento de video y configurarlo
+// DOM
 let live = document.getElementById("liveVideo");
 const video1 = document.getElementById("video1");
 const video2 = document.getElementById("video2");
@@ -9,15 +9,19 @@ const btn_video3 = document.getElementById("btn_video3");
 const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
 const btn_src_off = document.getElementById("btn_src_off");
+const btn_pause = document.getElementById("btn_pause");
+const btn_play = document.getElementById("btn_play");
+const btn_back = document.getElementById("btn_retroceder");
+const btn_advance = document.getElementById("btn_avanzar");
+const btn_fullscreen = document.getElementById("btn_pcompleta");
 
+const TEST_IMAGE_URL = "test.jpg";
 
 const STATE = {
     OFF: false,
     ON: true,
 }
 
-
-//-- Establecer las dimensiones de los vídeos
 live.width=420;
 live.height=200;
 video1.width=200;  
@@ -27,11 +31,6 @@ video2.height=100;
 video3.width=200;  
 video3.height=100;
 
-//-- Imagen de Test usada
-const TEST_IMAGE_URL = "test.png";
-
-//-- Imagen estática a mostrar cuando el video no
-//-- ha arrancado
 live.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
@@ -41,7 +40,7 @@ video3.poster = TEST_IMAGE_URL;
 let state = STATE.OFF;
 console.log("Estado inicial: " + state);
 
-//-- FUENTES-ON
+//-- Fuentes ON
 btn_src_on.onclick = () => {
 
   // Cambio de estado
@@ -49,12 +48,10 @@ btn_src_on.onclick = () => {
   console.log("Cambio de estado: " + state);
   console.log('Fuentes ON');
  
-  //-- Establecer fuentes
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
   video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
 
-  //-- Reproducción de vídeos desde el comienzo en silencio
   video1.currentTime = 0;
   video1.play();
   video1.muted = true;
@@ -67,10 +64,10 @@ btn_src_on.onclick = () => {
   video3.play();
   video3.muted = true;
 
-  //-- En la emisión en directo ponemos la imagen de prueba
   live.poster = TEST_IMAGE_URL;
 };
 
+//-- Fuentes OFF
 btn_src_off.onclick = () => {
     console.log('Fuentes OFF');
     state = STATE.OFF;
@@ -80,16 +77,24 @@ btn_src_off.onclick = () => {
     video3.src = TEST_IMAGE_URL;
 };
 
-//-- Botón de Test
+//-- Test
 btn_test.onclick = () => {
     console.log('test');
     live.src = TEST_IMAGE_URL;
+    live.poster = TEST_IMAGE_URL;
 };
 
-//-- Botón de Selección de la cámara 1
+//-- Selección de vídeo
 btn_video1.onclick = () => {
     if (state == STATE.ON){
         console.log('Vídeo 1 en directo');
+        // Evitar solapación con 'test'
+        live.poster = null;
+        console.log(live.readyState);
+        // Cargar vídeo
+        if(video1.readyState !== 4) {
+            video1.load();
+        }
         live.src = video1.src;
         live.currentTime = video1.currentTime;
         live.play();
@@ -101,6 +106,10 @@ btn_video1.onclick = () => {
 btn_video2.onclick = () => {
     if (state == STATE.ON){
         console.log('Vídeo 2 en directo');
+        live.poster = null;
+        if(video2.readyState !== 4) {
+            video2.load();
+        }
         live.src = video2.src;
         live.currentTime = video2.currentTime;
         live.play();
@@ -112,6 +121,10 @@ btn_video2.onclick = () => {
 btn_video3.onclick = () => {
     if (state == STATE.ON){
         console.log('Vídeo 3 en directo');
+        live.poster = null;
+        if(video3.readyState !== 4) {
+            video3.load();
+        }
         live.src = video3.src;
         live.currentTime = video3.currentTime;
         live.play();
@@ -119,3 +132,38 @@ btn_video3.onclick = () => {
         console.log('Primero hay que activar las fuentes');        
     }
 };
+
+btn_play.onclick = () => {
+    if(live.paused) {
+        live.play();
+    }
+};
+
+btn_pause.onclick = () => {
+    if(!live.paused) {
+        live.pause();
+    }
+};
+
+btn_advance.onclick = () => {
+    live.currentTime = live.currentTime + 2;
+    live.play();
+};
+
+btn_back.onclick = () => {
+    live.currentTime = live.currentTime - 2;
+    live.play();
+};
+
+btn_back.onclick = () => {
+    live.currentTime = live.currentTime - 2;
+    live.play();
+};
+
+btn_fullscreen.onclick = () => {
+    if(state == STATE.ON) {
+        live.requestFullscreen();
+        live.play();
+    }
+};
+
